be062efc0a5f9cc79c132e6357b3750c0a578b9f
Tran Ong <baotranongtran@gmail.com>
2024-06-09 00:40:04 -0400

    fix Try Birdie button

--- components/Hero.js
+++ components/Hero.js
@@ -1,8 +1,12 @@
+'use client'
+
 import Image from "next/image";
 import config from "@/config";
 import ButtonGradient from "./ButtonGradient";
+import { useRouter } from 'next/navigation'
 
 const Hero = () => {
+  const router = useRouter();
   return (
     <section className="max-w-7xl mx-auto bg-base-100 flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20">
       <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-center lg:text-center">
@@ -15,7 +19,7 @@ const Hero = () => {
           Get answers to your dietary questions, access personalized nutrition advice, and receive a custom meal plan from our AI, expertly trained using reputable medical sources and scientific journals.
         </p>
         <div className="flex flex-col lg:flex-row gap-4"> {/* Use flex row for side-by-side buttons */}
-          <button className="btn btn-primary btn-wide btn-orange-solid">
+          <button className="btn btn-primary btn-wide btn-orange-solid" onClick={() => router.push('/dashboard')}>
             Try {config.appName}
           </button>
           <a href="#faq" className="btn btn-secondary btn-wide btn-orange-border">
