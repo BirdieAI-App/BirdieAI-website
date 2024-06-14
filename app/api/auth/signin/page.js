9495ca3c62a08062c3f507552f12e61ad5b8caef
Tran Ong <baotranongtran@gmail.com>
2024-06-14 01:39:27 -0500

    fix next-auth.js to add credentials provider for email & password login

--- app/api/auth/signin/page.js
+++ app/api/auth/signin/page.js
@@ -3,23 +3,32 @@
 import { useEffect, useState } from 'react';
 import { getProviders, signIn } from 'next-auth/react';
 import Link from 'next/link';
-import { useSession } from 'next-auth/react';
+import { useSession } from 'next-auth/react'
+import { getCsrfToken } from "next-auth/react";
 
 export default function SignIn() {
     const [providers, setProviders] = useState(null);
+    const [csrfToken, setCsrfToken] = useState("");
 
     useEffect(() => {
         async function fetchProviders() {
             const res = await getProviders();
             setProviders(res);
+            console.log(res);
         }
 
+        const fetchCsrfToken = async () => {
+            const token = await getCsrfToken();
+            setCsrfToken(token);
+        };
+          
+        fetchCsrfToken();
         fetchProviders();
     }, []);
 
     if (!providers) {
         return <div>Loading...</div>;
-    }
+    } 
 
     return (
         <div className="flex items-center flex-col justify-center">
@@ -29,7 +38,9 @@ export default function SignIn() {
             </div>
 
             <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form class="space-y-6" action="#" method="POST">
+                {providers.credentials && 
+                (<form class="space-y-6" method="post" action="/api/auth/callback/credentials">
+                    <input name="csrfToken" type="hidden" defaultValue={csrfToken}/>
                     <div>
                         <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
                         <div class="mt-2">
@@ -52,15 +63,16 @@ export default function SignIn() {
                     <div>
                         <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
                     </div>
-                </form>
+                </form>)}
                 {providers.google && (
                 <button className="flex w-full justify-center py-1.5 border border-slate-200 rounded-lg text-slate-700 my-8 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150"
                 onClick={() => signIn(providers.google.id)}>
                     <img className="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo" />
-                    <span> Continue with Google</span>
+                    <span className='ml-3'> Continue with Google</span>
                 </button>
                 
                 )}
+                {/* {providers.credentials} */}
                 <p class="mt-10 text-center text-sm text-gray-500">
                     Not a member?
                     <Link href="/api/auth/signup" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"> Create account </Link>
@@ -70,4 +82,3 @@ export default function SignIn() {
         </div>
     );
 }
-
--- libs/next-auth.js
+++ libs/next-auth.js
@@ -4,6 +4,8 @@ import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import config from "@/config";
 import connectMongo from "./mongo";
 import { sendGoogleIDToken } from "./request";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { redirect } from "next/dist/server/api-utils";
 
 export const authOptions = {
   // Set any random key in .env.local
@@ -23,6 +25,34 @@ export const authOptions = {
         };
       },
     }),
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const {email, password} = credentials;
+        const user = { email: email, password: password};
+        // console.log(user);
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          // console.log(user);
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+  
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      }
+    }),
     // Follow the "Login with Email" tutorial to set up your email server
     // Requires a MongoDB database. Set MONOGODB_URI env variable.
     ...(connectMongo
@@ -38,12 +68,13 @@ export const authOptions = {
   // Requires a MongoDB database. Set MONOGODB_URI env variable.
   // Learn more about the model type: https://next-auth.js.org/v3/adapters/models
   ...(connectMongo && { adapter: MongoDBAdapter(connectMongo) }),
-  pages: {
-    signIn: '/api/auth/signin',  // Custom sign-in page
-  },
+  // pages: {
+  //   signIn: '/api/auth/signin',  // Custom sign-in page
+  // },
   callbacks: {
     async jwt({ token, account }) {
-      if (account) {
+      // console.log(account);
+      if (account?.provider === "google"){
           token.idToken = account.id_token;
           // console.log(account.id_token);
           const data = await sendGoogleIDToken(token);
@@ -51,17 +82,10 @@ export const authOptions = {
       }
       return token;
     },
-    // async session({ session, token }) {
-    //     if (session?.user) {
-    //         session.user.id = token.sub;
-    //         session.idToken = token.idToken;  // Add idToken to the session
-    //     }
-    //     return session;
-    // },
     async redirect({ url, baseUrl }) {
       // Redirect to a specific path after sign-in
       return baseUrl + "/dashboard";
-    },
+    }
   },
   session: {
     strategy: "jwt",
@@ -70,6 +94,6 @@ export const authOptions = {
     brandColor: config.colors.main,
     // Add you own logo below. Recommended size is rectangle (i.e. 200x50px) and show your logo + name.
     // It will be used in the login flow to display your logo. If you don't add it, it will look faded.
-    logo: `https://${config.domainName}/logoAndName.png`,
+    // logo: `https://${config.domainName}/logoAndName.png`,
   },
 };
