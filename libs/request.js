a4ceb150307bdf011dd5012bc6708923c02ac2ee
Kaia15 <baotranongtran@gmail.com>
2024-06-21 17:45:40 -0500

    logging base URL by apiClient

--- libs/request.js
+++ libs/request.js
@@ -12,6 +12,7 @@ export const getAllUSers = async function () {
 
 export const postUser = async function (payload) {
     // console.log(payload);
+    console.log('Base URL', apiClient.defaults.baseURL);
     const response = await apiClient.post('/users', payload);
     return response;
 }
