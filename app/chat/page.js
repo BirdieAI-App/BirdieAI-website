cc41d3c5978485512dc68ddd0e0b6113576e67df
tinnn1908-se <tinnn1908.se@gmail.com>
2024-06-24 22:26:58 -0500

    Mobile and Desktop for Chat AI

--- app/chat/page.js
+++ app/chat/page.js
@@ -3,45 +3,258 @@ import React, { useState } from 'react'
 
 const page = () => {
 
-  const [isChecked, setIsChecked] = useState(false)
+  const [isChecked, setIsChecked] = useState(true)
   function onClickHandler(event) {
     event.preventDefault();
     setIsChecked(true)
   }
 
   return (
-    <main className='h-screen grid grid-flow-row-dense grid-cols-5'>
-      <div class="col-span-1 grid grid-rows-15">
-        <div class="row-start-1 row-end-3 bg-yellow-500">
-          <div>New Chat</div>
-          <div>Previous Chat</div>
-          <div>Upgrade your account</div>
-        </div>
-        <div class="row-start-10 row-end-15 bg-orange-500">Profile Settings</div>
-      </div>
-      <div class="col-span-4 grid grid-rows-10 ">
-        {
-          isChecked ? (
-            <div className='row-start-1 row-end-8 grid grid-rows-10 bg-pink-500'>
-              <div className='row-start-1 row-end-2'>Welcome to Birdie - your diet coach for pregnancy,
-                postpartum and infant nutrition needs</div>
-              <div className='row-span-1 bg-red-500'>How can I help you today? Here are some ideas</div>
-              <div className='row-span-1 bg-purple-500'>I have a specific question about my medical condition related to nutrition </div>
-              <div className='row-span-1 bg-gray-500'>I want to have personalized nutrition plan for my condition</div>
-              <div className='row-span-1 bg-blue-500'>I want to some recipes tailors to my nutrition needs</div>
-              <div className='row-span-1 bg-orange-500'>I want a meal plan for my kids </div>
-            </div>
-          ) : (
-            <div>
-              Div B
-            </div>)
-        }
+    // <main className='h-screen grid grid-flow-row-dense grid-cols-6'>
+    //   <div class="col-span-1 grid grid-rows-10 ">
+    //     <div class="row-start-1 row-end-8 flex flex-col items-center">
+    //       <button className="btn bg-green-500 w-3/5 my-5 text-white text-center border-1 rounded-[3px] mx-auto">
+    //         <p className='text-[12px]'>+</p>
+    //         <p className='text-[12px]'>New Chat</p>
+    //       </button>
+    //       <div className="dropdown dropdown-bottom w-full my-5 text-left border-1 rounded-[3px]">
+    //         <div tabIndex={0} role="button" className="btn m-1 font-inter text-12px font-normal leading-[14.52px] text-left text-black">Previous Chat</div>
+    //         <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+    //           <li><a>Chat A</a></li>
+    //           <li><a>Chat B</a></li>
+    //         </ul>
+    //       </div>
+    //       <button className="w-full my-5 text-left border rounded-[3px] tracking-wide btn flex flex-col items-start">
+    //         <p className="font-inter text-[12px] font-normal leading-[14.52px] text-black">
+    //           Upgrade Your Account
+    //         </p>
+    //         <p className="font-inter text-[12px] font-normal leading-[14.52px]">
+    //           You have used {0} of 3 free chats
+    //         </p>
+    //       </button>
+    //     </div>
+    //     <div class="row-start-10  flex items-center justify-center space-x-2">
+    //       <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    //         <path d="M18 15.5C18.2761 15.5 18.5 15.2761 18.5 15C18.5 14.7239 18.2761 14.5 18 14.5V15.5ZM14.875 14.5C14.5989 14.5 14.375 14.7239 14.375 15C14.375 15.2761 14.5989 15.5 14.875 15.5V14.5ZM11.125 15.5C11.4011 15.5 11.625 15.2761 11.625 15C11.625 14.7239 11.4011 14.5 11.125 14.5V15.5ZM3 14.5C2.72386 14.5 2.5 14.7239 2.5 15C2.5 15.2761 2.72386 15.5 3 15.5V14.5ZM18 10.5C18.2761 10.5 18.5 10.2761 18.5 10C18.5 9.72386 18.2761 9.5 18 9.5V10.5ZM8.625 9.5C8.34886 9.5 8.125 9.72386 8.125 10C8.125 10.2761 8.34886 10.5 8.625 10.5V9.5ZM3 9.5C2.72386 9.5 2.5 9.72386 2.5 10C2.5 10.2761 2.72386 10.5 3 10.5V9.5ZM18 5.5C18.2761 5.5 18.5 5.27614 18.5 5C18.5 4.72386 18.2761 4.5 18 4.5V5.5ZM14.875 4.5C14.5989 4.5 14.375 4.72386 14.375 5C14.375 5.27614 14.5989 5.5 14.875 5.5V4.5ZM11.125 5.5C11.4011 5.5 11.625 5.27614 11.625 5C11.625 4.72386 11.4011 4.5 11.125 4.5V5.5ZM3 4.5C2.72386 4.5 2.5 4.72386 2.5 5C2.5 5.27614 2.72386 5.5 3 5.5V4.5ZM18 14.5H14.875V15.5H18V14.5ZM11.125 14.5H3V15.5H11.125V14.5ZM18 9.5H8.625V10.5H18V9.5ZM5.5 9.5H3V10.5H5.5V9.5ZM18 4.5H14.875V5.5H18V4.5ZM11.125 4.5H3V5.5H11.125V4.5ZM7.5 10C7.5 10.4142 7.16421 10.75 6.75 10.75V11.75C7.7165 11.75 8.5 10.9665 8.5 10H7.5ZM6.75 10.75C6.33579 10.75 6 10.4142 6 10H5C5 10.9665 5.7835 11.75 6.75 11.75V10.75ZM6 10C6 9.58579 6.33579 9.25 6.75 9.25V8.25C5.7835 8.25 5 9.0335 5 10H6ZM6.75 9.25C7.16421 9.25 7.5 9.58579 7.5 10H8.5C8.5 9.0335 7.7165 8.25 6.75 8.25V9.25ZM13.75 15C13.75 15.4142 13.4142 15.75 13 15.75V16.75C13.9665 16.75 14.75 15.9665 14.75 15H13.75ZM13 15.75C12.5858 15.75 12.25 15.4142 12.25 15H11.25C11.25 15.9665 12.0335 16.75 13 16.75V15.75ZM12.25 15C12.25 14.5858 12.5858 14.25 13 14.25V13.25C12.0335 13.25 11.25 14.0335 11.25 15H12.25ZM13 14.25C13.4142 14.25 13.75 14.5858 13.75 15H14.75C14.75 14.0335 13.9665 13.25 13 13.25V14.25ZM13.75 5C13.75 5.41421 13.4142 5.75 13 5.75V6.75C13.9665 6.75 14.75 5.9665 14.75 5H13.75ZM13 5.75C12.5858 5.75 12.25 5.41421 12.25 5H11.25C11.25 5.9665 12.0335 6.75 13 6.75V5.75ZM12.25 5C12.25 4.58579 12.5858 4.25 13 4.25V3.25C12.0335 3.25 11.25 4.0335 11.25 5H12.25ZM13 4.25C13.4142 4.25 13.75 4.58579 13.75 5H14.75C14.75 4.0335 13.9665 3.25 13 3.25V4.25Z" fill="#444444" />
+    //       </svg>
+    //       <p className='font-inter text-[12px] font-normal leading-[14.52px] text-left text-gray-custom'>Profile Setting</p>
+    //     </div>
+    //   </div>
+    //   <div class="col-start-3 col-end-6 grid grid-rows-4 ">
+    //     {
+    //       isChecked ? (
+    //         <div className="flex flex-col justify-center items-center row-start-2 row-end-4">
+    //           <div className="icon">
+    //             <img src='../icon.png' class="object-scale-down h-24 w-48 ..." />
+    //           </div>
+    //           <div className="flex justify-between ">
 
-        <div className='row-start-10 row-end-11 bg-pink-500'>
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={onClickHandler}>
-            Button
-          </button>
+    //             <div className="flex flex-col rounded-2xl p-4 cursor-pointer border border-transparent hover:border-gray-300">
+    //               <svg className="" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg" >
+    //                 <path d="M8.99994 3L1.99994 15L8.99994 13.2222L15.9999 15L8.99994 3ZM8.99994 3L8.99994 11.5" stroke="#7CD2EB" stroke-miterlimit="16" stroke-linecap="round" stroke-linejoin="round" />
+    //               </svg>
+    //               <p className="text-left ">
+    //                 I have a specific question about my medical condition related to nutrition
+    //               </p>
+    //             </div>
+
+    //             <div className="flex flex-col rounded-2xl p-4 cursor-pointer border border-transparent hover:border-gray-300">
+    //               <svg className="" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg" >
+    //                 <path d="M8.99994 3L1.99994 15L8.99994 13.2222L15.9999 15L8.99994 3ZM8.99994 3L8.99994 11.5" stroke="#7CD2EB" stroke-miterlimit="16" stroke-linecap="round" stroke-linejoin="round" />
+    //               </svg>
+    //               <p className="text-left ">
+    //                 I have a specific question about my medical condition related to nutrition
+    //               </p>
+    //             </div>
+
+    //             <div className="flex flex-col rounded-2xl p-4 cursor-pointer border border-transparent hover:border-gray-300">
+    //               <svg className="" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg" >
+    //                 <path d="M8.99994 3L1.99994 15L8.99994 13.2222L15.9999 15L8.99994 3ZM8.99994 3L8.99994 11.5" stroke="#7CD2EB" stroke-miterlimit="16" stroke-linecap="round" stroke-linejoin="round" />
+    //               </svg>
+    //               <p className="text-left ">
+    //                 I have a specific question about my medical condition related to nutrition
+    //               </p>
+    //             </div>
+
+    //             <div className="flex flex-col rounded-2xl p-4 cursor-pointer border border-transparent hover:border-gray-300">
+    //               <svg className="" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg" >
+    //                 <path d="M8.99994 3L1.99994 15L8.99994 13.2222L15.9999 15L8.99994 3ZM8.99994 3L8.99994 11.5" stroke="#7CD2EB" stroke-miterlimit="16" stroke-linecap="round" stroke-linejoin="round" />
+    //               </svg>
+    //               <p className="text-left ">
+    //                 I have a specific question about my medical condition related to nutrition
+    //               </p>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       ) : (
+    //         <div>
+    //           Div B
+    //         </div>)
+    //     }
+
+    //     <div className='row-start-6 w-full flex items-center justify-between p-4'>
+    //       {/* <img src='../icon.png' class="object-scale-down h-12 w-24 ..." /> */}
+    //       {/* <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={onClickHandler}>
+    //         Button
+    //       </button> */}
+    //       <input type="text" placeholder="Ask A Question" className="w-3/4 p-2 focus:outline-none rounded-md" />
+
+    //       <button className='btn' onClick={onClickHandler}>
+    //         <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg" >
+    //           <path d="M8.99994 3L1.99994 15L8.99994 13.2222L15.9999 15L8.99994 3ZM8.99994 3L8.99994 11.5" stroke="#444444" stroke-miterlimit="16" stroke-linecap="round" stroke-linejoin="round" />
+    //         </svg>
+    //       </button>
+    //     </div>
+    //   </div>
+    // </main>
+
+    <main className="main grid grid-rows-12 h-screen w-screen
+     md:grid md:grid-cols-4 md:h-screen md:w-screen">
+      {/**First | Left */}
+      <div className="row-span-1 md:col-span-1 pt-5
+       md:grid md:grid-rows-12 md:h-screen">
+        {/** Mobile : start*/}
+        <div className='flex items-center justify-center md:hidden'>
+          <svg className='w-1/3 md:hidden' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+            fill="none" stroke="currentColor" strokeWidth="2" S
+            strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-square-plus">
+            <rect width="18" height="18" x="3" y="3" rx="2" /><path d="M8 12h8" /><path d="M12 8v8" /></svg>
+          <img className=' object-contain h-12 w-1/3 md:hidden' src='../icon.png' />
+          <svg className='w-1/3 md:hidden' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+            fill="none" stroke="currentColor" stroke-width="2" strokeLinecap="round" strokeLinejoin="round"
+            class="lucide lucide-square-user"><rect width="18" height="18" x="3" y="3" rx="2" /><circle cx="12" cy="10" r="3" />
+            <path d="M7 21v-2a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v2" /></svg>
         </div>
+        {/** Mobile : End*/}
+        
+        {/** New Chat : Start*/}
+        <div className='hidden md:row-start-2 md:row-span-1 
+        md:grid md:grid-cols-3
+        w-2/3'>
+          <div className='md:col-start-2 md:col-span-2 bg-green-500
+          md:grid md:grid-cols-4 '>
+            <div className='md:col-start-1 md:col-span-1 md:flex md:items-center md:justify-center'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                fill="none" stroke="currentColor" strokeWidth="2" S
+                strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-square-plus">
+                <rect width="18" height="18" x="3" y="3" rx="2" /><path d="M8 12h8" /><path d="M12 8v8" /></svg>
+            </div>
+            <p className='md:col-start-2 md:col-span-3 md:flex md:items-center md:justify-center'>New Chat</p>
+          </div>
+        </div>
+        {/** New Chat : End*/}
+
+        {/** Profile : Start*/}
+        <div className='hidden md:row-start-11 md:row-span-1 
+        md:grid md:grid-cols-3
+         w-2/3'>
+          <div className='hidden md:col-start-2 md:col-span-2 bg-green-500
+          md:grid md:grid-cols-4 '>
+            <div className='hidden md:col-start-1 md:col-span-1 md:flex md:items-center md:justify-center'>
+              <svg className='hidden md:block' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                strokeLinejoin="round" class="lucide lucide-sliders-horizontal"><line x1="21" x2="14" y1="4" y2="4" /><line x1="10" x2="3" y1="4" y2="4" /><line x1="21" x2="12" y1="12" y2="12" /><line x1="8" x2="3" y1="12" y2="12" /><line x1="21" x2="16" y1="20" y2="20" /><line x1="12" x2="3" y1="20" y2="20" /><line x1="14" x2="14" y1="2" y2="6" /><line x1="8" x2="8" y1="10" y2="14" /><line x1="16" x2="16" y1="18" y2="22" /></svg>
+            </div>
+            <p className='hidden md:col-start-2 md:col-span-2 md:flex md:items-center md:justify-center'>Profile</p>
+          </div>
+        </div>
+        {/** Profile : End*/}
+      </div>
+
+      {/** Second | Row */}
+      <div className="row-span-11 md:col-span-3 md:h-screen
+      grid grid-rows-12">
+        {/**Suggestion: Start */}
+        <div className='row-start-1 row-end-11
+        grid grid-rows-12 p-5  '>
+          {/**Box: Start */}
+          <div className='hidden row-start-1 row-span-2 '></div>
+          {/**Box: End */}
+
+          {/**Box: Start */}
+          <div className='row-start-3 row-span-2  flex items-center justify-center border border-gray-400'>.
+            <svg className="w-1/4" width="24" height="24" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg" >
+              <path d="M8.99994 3L1.99994 15L8.99994 13.2222L15.9999 15L8.99994 3ZM8.99994 3L8.99994 11.5" stroke="#7CD2EB" stroke-miterlimit="16" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+            <p className="text-left w-2/4">
+              I have a specific question about my medical condition related to nutrition
+            </p>
+            <svg className="w-1/4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+              fill="none" stroke="currentColor"
+              strokeWidth="2" strokeLinecap="round"
+              strokeLinejoin="round" class="lucide lucide-chevron-right">
+              <path d="m9 18 6-6-6-6" /></svg>
+          </div>
+          {/**Box: End */}
+
+
+          {/**Box: Start */}
+          <div className='row-start-5 row-span-2  flex items-center justify-center border-gray-400 border-l border-r border-b'>.
+            <svg className="w-1/4" width="24" height="24" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg" >
+              <path d="M8.99994 3L1.99994 15L8.99994 13.2222L15.9999 15L8.99994 3ZM8.99994 3L8.99994 11.5" stroke="#7CD2EB" stroke-miterlimit="16" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+            <p className="text-left w-2/4">
+              I have a specific question about my medical condition related to nutrition
+            </p>
+            <svg className="w-1/4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+              fill="none" stroke="currentColor"
+              strokeWidth="2" strokeLinecap="round"
+              strokeLinejoin="round" class="lucide lucide-chevron-right">
+              <path d="m9 18 6-6-6-6" /></svg>
+          </div>
+          {/**Box: End */}
+
+          {/**Box: Start */}
+          <div className='row-start-7 row-span-2  flex items-center justify-center border-gray-400 border-l border-r border-b'>.
+            <svg className="w-1/4" width="24" height="24" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg" >
+              <path d="M8.99994 3L1.99994 15L8.99994 13.2222L15.9999 15L8.99994 3ZM8.99994 3L8.99994 11.5" stroke="#7CD2EB" stroke-miterlimit="16" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+            <p className="text-left w-2/4">
+              I have a specific question about my medical condition related to nutrition
+            </p>
+            <svg className="w-1/4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+              fill="none" stroke="currentColor"
+              strokeWidth="2" strokeLinecap="round"
+              strokeLinejoin="round" class="lucide lucide-chevron-right">
+              <path d="m9 18 6-6-6-6" /></svg>
+          </div>
+          {/**Box: End */}
+
+          {/**Box: Start */}
+          <div className='row-start-9 row-span-2  flex items-center justify-center border-gray-400 border-l border-r border-b'>.
+            <svg className="w-1/4" width="24" height="24" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg" >
+              <path d="M8.99994 3L1.99994 15L8.99994 13.2222L15.9999 15L8.99994 3ZM8.99994 3L8.99994 11.5" stroke="#7CD2EB" stroke-miterlimit="16" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+            <p className="text-left w-2/4">
+              I have a specific question about my medical condition related to nutrition
+            </p>
+            <svg className="w-1/4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+              fill="none" stroke="currentColor"
+              strokeWidth="2" strokeLinecap="round"
+              strokeLinejoin="round" class="lucide lucide-chevron-right">
+              <path d="m9 18 6-6-6-6" /></svg>
+          </div>
+          {/**Box: End */}
+
+
+        </div>
+        {/**Suggestion: End */}
+
+        {/**ChatBox: Start */}
+        <div className='row-start-11 flex items-center justify-center'>
+          <div className='flex w-2/3 h-full items-center justify-center'>
+
+            <input placeholder='Ask me question' className='w-3/5' />
+            <svg className='w-1/5' xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+              viewBox="0 0 24 24" fill="none" stroke="currentColor"
+              strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+              class="lucide lucide-send">
+              <path d="m22 2-7 20-4-9-9-4Z" /><path d="M22 2 11 13" /></svg>
+          </div>
+        </div>
+        {/**ChatBox: End */}
+        {/** */}
+        <div className='row-start-12 flex items-center justify-center'>Wonder to have sth here !</div>
       </div>
     </main>
   )
