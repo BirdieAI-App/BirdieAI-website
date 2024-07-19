6ba6a28097ab977533166c32dea4fb6b659cd269
Kaia15 <baotranongtran@gmail.com>
2024-07-19 16:09:58 -0500

    fix mobile screen & Load More button

--- components/Chat.js
+++ components/Chat.js
@@ -13,7 +13,7 @@ const Chat = () => {
   const [allThreads, setAllThreads] = useState([]);
   const [userId, setUserId] = useState("");
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [paginatedThreads, setPaginatedThreads] = useState({data: [], nextPage: null});
+  const [paginatedThreads, setPaginatedThreads] = useState({ data: [], nextPage: null });
 
   const router = useRouter();
   const suggestions = [
@@ -30,13 +30,13 @@ const Chat = () => {
       } catch (err) {
         console.log(err);
       }
-      
+
     }
   }
 
-  const getThreadsPaginated = async (page,data) => {
+  const getThreadsPaginated = async (page, data) => {
     try {
-      const threads = await getAllThreadsByUserPaginated(page,data);
+      const threads = await getAllThreadsByUserPaginated(page, data);
       // console.log(threads);
       setPaginatedThreads(previousResponse => {
         if (previousResponse === null) {
@@ -45,7 +45,7 @@ const Chat = () => {
         return { data: [...previousResponse.data, ...threads.data], nextPage: threads.nextPage }
       })
     } catch (err) {
-        console.log(err);
+      console.log(err);
     }
   }
 
@@ -75,7 +75,7 @@ const Chat = () => {
   }, [userId]);
 
   useEffect(() => {
-    allThreads && getThreadsPaginated(0,allThreads);
+    allThreads && getThreadsPaginated(0, allThreads);
   }, [allThreads])
 
   // console.log(allThreads);
@@ -97,37 +97,35 @@ const Chat = () => {
     setIsSidebarOpen(false);
   };
 
+  // console.log(paginatedThreads);
+
   return (
-    <div className="flex h-screen font-sans relative">
+    <div className="flex flex-col h-screen font-sans relative">
       <aside className={`fixed inset-y-0 left-0 w-64 bg-gray-100 p-5 flex flex-col transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} lg:translate-x-0 transition-transform duration-300 ease-in-out z-50 overflow-scroll`}>
-        <button className="bg-green-500 text-white py-2 px-4 rounded mb-5">New Chat</button>
+        <button className="bg-green-500 text-white py-2 px-4 rounded-lg mb-5">New Chat</button>
         <div className="mb-5">
           <ButtonAccount />
           <span className="block mt-3 mb-3">You have used 2 of 3 free chats.</span>
           <button className="bg-orange-500 text-white py-2 px-4 rounded">Upgrade for less than $10 / month</button>
         </div>
         <div className="mb-3 flex flex-col">
-          <h4 className="mb-2">Previous Chats</h4>
+          <h4 className={`mb-2 ${(allThreads.length > 5) ? "" : "hidden"}`}>Previous Chats</h4>
           {paginatedThreads.data?.map((item, idx) => (
             <button key={idx} className="text-black py-3 px-2 border border-gray-300 rounded mb-3 text-center">{item?.title}</button>
           ))}
         </div>
-        <button className="text-black py-3 px-2 border border-gray-300 rounded mb-3 text-center bg-white"
-        disabled={!paginatedThreads.nextPage}
-        onClick={() => {
-          paginatedThreads.nextPage && getThreadsPaginated(paginatedThreads.nextPage,allThreads);
-        }}>
+        <button className={`text-white py-3 px-2 rounded-lg mb-3 text-center bg-green-500 ${((allThreads.length < 5) || (paginatedThreads.nextPage === null)) ? "hidden" : ""}`}
+          onClick={() => {
+            paginatedThreads.nextPage && getThreadsPaginated(paginatedThreads.nextPage, allThreads);
+          }}>
           Load more
         </button>
-        {/* <div>
-          <h4 className="mb-2">Docs</h4>
-        </div> */}
       </aside>
 
       {/* Overlay for small screens */}
       {isSidebarOpen && <div className="fixed inset-0 bg-black opacity-50 z-40 lg:hidden" onClick={closeSidebar}></div>}
 
-      <div className="flex flex-col lg:hidden p-5">
+      <div className="flex lg:hidden p-5">
         <button onClick={toggleSidebar} className="text-2xl p-2 focus:outline-none">
           ☰
         </button>
@@ -135,14 +133,14 @@ const Chat = () => {
 
       <main className="flex-1 flex flex-col p-5 items-center lg:ml-64">
         <div className="flex justify-center mb-5">
-          <img className="h-20 w-20" src="../icon.png" />
+          <img className="h-20 w-20" src="../icon.png" alt="Birdie Logo" />
         </div>
         <header className="mb-5">
           <h1 className="text-center">How can I help you?</h1>
         </header>
         <div className="flex flex-col flex-1 items-center w-full">
           {suggestions?.map((item, idx) => (
-            <button key={idx} className="text-black py-3 px-2 border border-gray-300 rounded mb-3 text-center w-1/3">{item}</button>
+            <button key={idx} className="text-black py-3 px-2 border border-gray-300 rounded mb-3 text-center w-full md:w-1/2 lg:w-1/3">{item}</button>
           ))}
         </div>
         <div className="flex items-center mt-5 w-full md:w-3/4 lg:w-3/2">
@@ -157,11 +155,12 @@ const Chat = () => {
         </div>
         <footer className="mt-auto text-center text-gray-600 text-sm py-5">
           <span>
-          Birdie retrieved information from Library National of Medicine research papers, USDA Nutrition Guideline. Please consult the  healthcare providers for medical advice.
+            Birdie retrieved information from Library National of Medicine research papers, USDA Nutrition Guideline. Please consult the healthcare providers for medical advice.
           </span>
         </footer>
       </main>
     </div>
+
   );
 };
 
