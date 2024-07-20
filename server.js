696f9eabc9787c4900f9558eded1b6633dcce538
Tran Ong <baotranongtran@gmail.com>
2024-07-20 10:40:35 -0500

    add Allowed-Cors-Origin

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const authRoute = require('./backend/routes/authRoute.js');
 const threadRoute = require('./backend/routes/threadRoute.js');
 const messageRoute = require('./backend/routes/messageRoute.js');
 
-const corsOrigin = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN : "*";
-console.log(corsOrigin);
+// const corsOrigin = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN : "*";
+// console.log(corsOrigin);
 
 // Load environment variables from .env file
 dotenv.config({ path: path.resolve(__dirname, './.env') });
@@ -33,15 +33,20 @@ app.use((req, res, next) => {
 })
 
 // CORS middleware
-const corsOptions = {
-	origin: corsOrigin, // Ensure this environment variable is set
-	methods: ['GET', 'POST', 'PUT', 'DELETE'],
-	allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'X-XSRF-TOKEN', 'Accept', 'Origin'],
-	credentials: true,
-	optionsSuccessStatus: 200 // Some legacy browsers choke on a 204 status
-  };
+// const corsOptions = {
+// 	origin: corsOrigin, // Ensure this environment variable is set
+// 	methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// 	allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'X-XSRF-TOKEN', 'Accept', 'Origin'],
+// 	credentials: true,
+// 	optionsSuccessStatus: 200 // Some legacy browsers choke on a 204 status
+//   };
 
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	next();
+  });
+
+app.use(cors());
 
 
 //making connection to MongoDB
