7955a9a2555775f44c31e57cea9e5cfe00c9cffc
Kaia15 <baotranongtran@gmail.com>
2024-08-10 11:19:43 -0500

    re-check before pushing

--- hooks/useChat.js
+++ hooks/useChat.js
@@ -5,18 +5,20 @@ import config from "@/config";
 import axios from "axios";
 import OpenAI from "openai";
 
-const OPENAI_API_KEY = "**";
-const CONTENT = "**"
+const OPENAI_API_KEY="sk-proj-8Kraawye8AQDEMZSUbZmT3BlbkFJUJbHf4zcbN3imhQL31xJ";
+// ASST_ID = asst_gqwuEwTDxy0u47BhXaQjfV3B
+const OPENAI_PROMPT = "As Diet Coach, your interactions are collaborative, informative, deeply rooted in trustworthiness, transparent, and focused strictly on nutrition and diet. You start each session by introducing yourself as a dietitian here to assist with dietary concerns and goals within the realms of Prenatal Nutrition, Postpartum Nutrition, and Pediatric Nutrition. Your answer structure includes clarifying user queries, explaining the condition and symptoms using PubMed and Mayo Clinic website, providing dietary advice and nutrition requirements based on “Dietary Guidelines for Americans, 2020-2025” research, and offering practical recipes following Myplate website. When a user asks about a specific medical symptom, you will respond in the structure: \nremind users that the answer is only limited to pregnancy, postpartum, and pediatric diet topics. Advise consulting healthcare providers for medical assistance. \nProvide evidence that certain food ingredients can help reduce the symptom, including citations. \nProvide some recipes that include these food ingredients. \nFor all answers, mention how many sites you searched to get the answer and provide links to these sites. \n none"
 
 export function useChat() {
     const [currentResponse, setCurrentResponse] = useState("");
     const [streaming, setStreaming] = useState(false);
     const [sentFirstMessage, setSentFirstMessage] = useState(false);
+    // console.log(process.env.OPENAI_API_KEY);
 
     const [conversation, setConversation] = useState([
         {
             role: "system",
-            content: CONTENT,
+            content: OPENAI_PROMPT,
         },
     ]);
     const [message, setMessage] = useState("");
@@ -28,6 +30,7 @@ export function useChat() {
 
     async function handleOnClick() {
         if (!sentFirstMessage) setSentFirstMessage(true);
+        setMessage("");
         const newConversation = [
             ...conversation,
             {
