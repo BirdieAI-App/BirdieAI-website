ae0b828886a8a04fec35bbbc801d40e4c1ac51d2
Yen Le <163058505+yenle7@users.noreply.github.com>
2024-09-23 17:15:11 -0700

    Revert "YenLe"

--- app/test-openAI/page.js
+++ app/test-openAI/page.js
@@ -76,7 +76,7 @@ const StreamingComponent = () => {
   const [message, setMessage] = useState("");
 
   const openai = new OpenAI({
-    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
     dangerouslyAllowBrowser: true,
   });
 
