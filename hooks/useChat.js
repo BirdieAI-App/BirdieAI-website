c3b5f944c030fe9eda3196519272e1150b12f323
kaia15 <baotranongtran@gmail.com>
2024-11-03 16:51:54 -0500

    commit last updates

--- hooks/useChat.js
+++ hooks/useChat.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 import OpenAI from "openai";
 import { extractFirstFourWords } from "@/libs/util";
