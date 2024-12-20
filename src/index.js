6a16afe94ddfeccedb20c1696f7ffd65749ce64c
DungLe2911 <lehoangdung29111998@gmail.com>
2024-12-20 17:03:25 -0500

    Update backend to store secret in AWS Secret Manager

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import serverless from 'serverless-http';
 import cors from 'cors';
 import mongoose from 'mongoose';
-
+import AWS from 'aws-sdk';
 // import routes
 import userRoute from './backend/routes/UserRoute.js'
 import threadRoute from './backend/routes/threadRoute.js';
@@ -21,17 +21,45 @@ app.use(express.json()); // Parse JSON bodies
 
 // CORS middleware
 const corsOptions = {
-	origin: "*", // Ensure this environment variable is set
-	methods: ['GET', 'POST', 'PUT', 'DELETE'],
-	allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'X-XSRF-TOKEN', 'Accept', 'Origin'],
-	credentials: true,
-	optionsSuccessStatus: 200 // Some legacy browsers choke on a 204 status
+  origin: "*", // Ensure this environment variable is set
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'X-XSRF-TOKEN', 'Accept', 'Origin'],
+  credentials: true,
+  optionsSuccessStatus: 200 // Some legacy browsers choke on a 204 status
 };
 app.use(cors(corsOptions));
 
+
+try {
+  // In Lambda, no need to configure credentials - uses Lambda role automatically
+  const secretsManager = new AWS.SecretsManager({
+    region: 'us-east-2'
+  });
+
+  const response = await secretsManager.getSecretValue({
+    SecretId: 'dev/birdieAI/backend'
+  }).promise();
+
+  const secrets = JSON.parse(response.SecretString);
+
+  // Merge secrets with process.env
+  process.env = {
+    ...process.env,
+    STRIPE_SECRET_KEY: secrets.STRIPE_SECRET_KEY,
+    GOOGLE_ID: secrets.GOOGLE_ID,
+    MONGODB_URI: secrets.MONGODB_URI
+  };
+  console.log('AWS Secrets loaded successfully');
+} catch (err) {
+  const message = `"Error while retriving credentials from secret manager: ${err.message}`;
+  console.error(message);
+  throw new Error(message);
+}
+
+
 try {
   if (mongoose.connection.readyState !== 1) {
-    await mongoose.connect( process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('Database connected successfully');
   }
 } catch (error) {
