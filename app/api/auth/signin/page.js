b0dc92077ed63240e8938da5e7a6c4ecdff1c65c
DungLe2911 <lehoangdung29111998@gmail.com>
2025-01-17 16:40:10 -0500

    Added Signin With google

--- app/api/auth/signin/page.js
+++ app/api/auth/signin/page.js
@@ -27,7 +27,7 @@ export default function SignIn() {
     //         const token = await getCsrfToken();
     //         setCsrfToken(token);
     //     };
-          
+
     //     fetchCsrfToken();
     //     fetchProviders();
     // }, []);
@@ -70,10 +70,10 @@ export default function SignIn() {
     //     }
     // };
 
-    const handleSignInWithGoogle = async () =>{
-        try{
-            const response = await SignInGoogle();
-        }catch(err){
+    const handleSignInWithGoogle = async () => {
+        try {
+            window.location.href = SignInGoogle();
+        } catch (err) {
             console.log(err.message);
         }
     }
@@ -84,48 +84,54 @@ export default function SignIn() {
                 <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">Sign in to your account</h1>
-                            <button className="flex w-full justify-center py-1.5 border border-slate-200 rounded-lg text-slate-700 my-8 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150"
-                            onClick={() => handleSignInWithGoogle()}>
-                                <img className="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo" />
-                                <span className='ml-3'> Continue with Google</span>
-                            </button>
+                        <Link href={`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/google`} passHref>
+                            <div className="flex w-full justify-center py-1.5 border border-slate-200 rounded-lg text-slate-700 my-8 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150">
+                                <img
+                                    className="w-6 h-6"
+                                    src="https://www.svgrepo.com/show/475656/google-color.svg"
+                                    loading="lazy"
+                                    alt="google logo"
+                                />
+                                <span className="ml-3">Continue with Google</span>
+                            </div>
+                        </Link>
                         <div className="inline-flex items-center justify-center w-full">
-                            <hr className="w-full h-px my-8 bg-gray-200 border-0"/>
+                            <hr className="w-full h-px my-8 bg-gray-200 border-0" />
                             <span className="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2">or</span>
                         </div>
-                            <form className="space-y-6" method="post" onSubmit={console.log("submit credentials")}>
-                                <input name="csrfToken" type="hidden"/>
-                                <div>
-                                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-                                    <div className="mt-2">
-                                        <input id="email" name="email" type="email" autoComplete="email" 
-                                        required className="pl-2 bg-white block w-full rounded-md border border-gray-300 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+                        <form className="space-y-6" method="post" onSubmit={console.log("submit credentials")}>
+                            <input name="csrfToken" type="hidden" />
+                            <div>
+                                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                                <div className="mt-2">
+                                    <input id="email" name="email" type="email" autoComplete="email"
+                                        required className="pl-2 bg-white block w-full rounded-md border border-gray-300 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         placeholder="name@company.com"
-                                        />
-                                    </div>
+                                    />
                                 </div>
+                            </div>
 
-                                <div>
-                                    <div className="flex items-center justify-between">
-                                        <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                                        <div className="text-sm">
-                                            <Link href="/forgot-password" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</Link>
-                                        </div>
-                                    </div>
-                                    <div className="mt-2">
-                                        <input id="password" name="password" type="password" autoComplete="current-password" 
-                                        required className="pl-2 bg-white block w-full rounded-md border border-gray-300 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
-                                        placeholder="••••••••"
-                                        />
+                            <div>
+                                <div className="flex items-center justify-between">
+                                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                                    <div className="text-sm">
+                                        <Link href="/forgot-password" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</Link>
                                     </div>
                                 </div>
-                                <div>
-                                    <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                <div className="mt-2">
+                                    <input id="password" name="password" type="password" autoComplete="current-password"
+                                        required className="pl-2 bg-white block w-full rounded-md border border-gray-300 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        placeholder="••••••••"
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                     disabled={loading}>
                                     {loading ? 'Signing in...' : 'Sign in'}
-                                    </button>
-                                </div>
-                            </form>
+                                </button>
+                            </div>
+                        </form>
                         <p className="mt-10 text-center text-sm text-gray-500">
                             Not a member?
                             <Link href="/signup" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"> Create account </Link>
