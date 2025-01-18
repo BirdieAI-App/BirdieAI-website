8c8b0cf5b3a702553d7c161e2e836ca14f3f7bce
DungLe2911 <lehoangdung29111998@gmail.com>
2025-01-17 21:01:49 -0500

    Missing ?

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-XSRF-TOKEN, Accept, Origin');
 
   if(!req.headers.authorization){
-    const token = req.cookies.jwt;
+    const token = req.cookies?.jwt;
     if (token) {
       req.headers.authorization = token;
     }
